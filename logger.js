f34560e08db008ebe89b4b9e3aab01eac6f5417e
unknown <finnneron1@gmail.com>
2022-02-22 20:10:01 +0300

    color-fixes

--- logger.js
+++ logger.js
@@ -19,7 +19,7 @@ const genId = () => {
 }
 
 //filename
-let filename = (getTime()+"_"+genId()).replace(" ", "-")+".json";
+let filename = (getTime()+"_"+genId()).replace(/[^a-zA-Z0-9]/g,'_').replace(/__/g,'_').replace(" ", "-")+".json";
 
 //formatter config
 let config = {
