6402a506614bf6e4a302260db925cacdc00b1ff0
unknown <finnneron1@gmail.com>
2022-02-22 19:53:03 +0300

    progress

--- logger.js
+++ logger.js
@@ -5,3 +5,21 @@ let fs = require('fs');
 const getTime = () => {
     return new Date(Date.now()).toLocaleString("en-US", { timeZone: "Africa/Nairobi" });
 }
+
+//today's date
+const todayDate = () => {
+    const today = new Date();
+    let todaydate = "log"+today.getDate()+"-"+(today.getMonth()+1)+"-"+today.getFullYear();
+    return todaydate
+}
+
+//generate unique id
+const genId = () => {
+    return (new Date()).getTime().toString(36) + Math.random().toString(36).slice(2);
+}
+
+//formatter config
+let config = {
+    type: 'space',
+    size: 2
+}
